d45f273af6bc13cfdf0a5f066efa948645856314
Antonio Aragon <antonio.aragon@unimi.it>
2024-10-24 11:56:47 +0200

    new data division discards unlabeled test samples

--- download_data.py
+++ download_data.py
@@ -18,8 +18,70 @@ with zipfile.ZipFile('dogs-vs-cats.zip', 'r') as zip_ref:
 
 with zipfile.ZipFile('data/train.zip', 'r') as zip_ref:
     zip_ref.extractall('data')
+    os.rename('data/train', 'data/full_dataset')
 
-with zipfile.ZipFile('data/test1.zip', 'r') as zip_ref:
-    zip_ref.extractall('data')
+
+# %%
+
+# split train data into train and test sets
+import os
+import glob
+import shutil
+import random
+import tqdm
+
+
+# remove previous data folders and children
+if os.path.exists('data/train'):
+    shutil.rmtree('data/train')
+if os.path.exists('data/test'):
+    shutil.rmtree('data/test')
+
+
+# Create directories
+os.makedirs('data/train/cats')
+os.makedirs('data/train/dogs')
+os.makedirs('data/test/cats')
+os.makedirs('data/test/dogs')
+
+
+seed = 42
+random.seed(seed)
+
+# Get all files
+cat_files = glob.glob('data/full_dataset/cat.*.jpg')
+dog_files = glob.glob('data/full_dataset/dog.*.jpg')
+
+# Shuffle files
+random.shuffle(cat_files)
+random.shuffle(dog_files)
+
+# Split files
+train_cat_files = cat_files[:int(0.8*len(cat_files))]
+test_cat_files = cat_files[int(0.8*len(cat_files)):]
+train_dog_files = dog_files[:int(0.8*len(dog_files))]
+test_dog_files = dog_files[int(0.8*len(dog_files)):]
+
+
+# copy them to the corresponding folders
+for file in tqdm.tqdm(train_cat_files):
+    shutil.copy(file, 'data/train/cats')
+
+for file in tqdm.tqdm(test_cat_files):
+    shutil.copy(file, 'data/test/cats')
+
+for file in tqdm.tqdm(train_dog_files):
+    shutil.copy(file, 'data/train/dogs')
+
+for file in tqdm.tqdm(test_dog_files):
+    shutil.copy(file, 'data/test/dogs')
+
+
+
+# %%
+print(f"Number of training cat images: {len(train_cat_files)}")
+print(f"Number of test cat images: {len(test_cat_files)}")
+print(f"Number of training dog images: {len(train_dog_files)}")
+print(f"Number of test dog images: {len(test_dog_files)}")
 
 # %%
